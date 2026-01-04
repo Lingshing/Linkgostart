01f51aee50cc771a82e47b2b1abcfc270befd348
Lingshing <138836434+Lingshing@users.noreply.github.com>
2026-01-05 00:21:03 +0800

    Update OneSignalSDKWorker.js

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -1,1 +1,4 @@
-importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDKWorker.js');
+/* 本地化 OneSignal Worker */
+if (typeof self.importScripts === 'function') {
+    self.importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
+}
